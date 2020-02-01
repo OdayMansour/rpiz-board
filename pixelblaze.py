e106a5cace0048458ee36525c52c660a5747897e
Oday Mansour <odaym@nsour.net>
2020-02-01 21:45:32 +0000

    Pixelblaze patterns, code to retrieve and set pattern

---
+++ pixelblaze.py
@@ -0,0 +1,41 @@
+from websocket import create_connection
+import random
+
+def send_and_receive(url, message):
+  ws = create_connection(url)
+  ws.send(message)
+  return ws.recv()
+  ws.close()
+
+
+def send_and_forget(url, message):
+  ws = create_connection(url)
+  ws.send(message)
+  ws.close()
+
+
+ip = '192.168.1.19'
+port = '81'
+url = 'ws://' + ip + ':' + port + '/'
+
+print 'Listing programs...'
+result = send_and_receive(url, '{"listPrograms": true}')
+
+print 'Got programs, parsing...'
+trimmed_result = result[2:-1] # Removing first two binary bytes and last newline
+programs = []
+
+for program in trimmed_result.split('\n'):
+  id = program.split('\t')[0]
+  name = program.split('\t')[1]
+  programs.append({"id": id, "name": name})
+
+print 'Found programs: '
+print programs
+
+print 'Selecting a program at random...'
+selected = programs[random.randrange(len(programs))]
+print 'Selected ' + selected["id"] + ' - ' + selected["name"]
+
+send_and_forget(url, '{"activeProgramId": "' + selected["id"] + '"}')
+
