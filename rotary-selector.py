128013681865aa4f9003a782b42b5f2220e1adaf
Oday Mansour <odaym@nsour.net>
2020-02-08 01:17:47 +0000

    Listening to switch too

--- rotary-selector.py
+++ rotary-selector.py
@@ -18,46 +18,54 @@ def send_and_forget(url, message):
 
 def select(url, selection):
   message = '{"setVars": {"selected": ' + str(selection) + '}}'
-  send_and_forget(url, message)
+  # send_and_forget(url, message)
 
 
 ip = '192.168.1.19'
 port = '81'
 url = 'ws://' + ip + ':' + port + '/'
-send_and_forget(url, '{"activeProgramId": "Xv9GdRrNxTRSkZhba"}')
+#send_and_forget(url, '{"activeProgramId": "Xv9GdRrNxTRSkZhba"}')
 
-clk = 21
+cl = 21
 dt = 16
+sw = 20
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(cl, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 counter = 0
 
-clkState_last = GPIO.input(clk)
+clState_last = GPIO.input(cl)
 dtState_last = GPIO.input(dt)
 
 try:
 
   while True:
-    clkState = GPIO.input(clk)
+    clState = GPIO.input(cl)
     dtState = GPIO.input(dt)
+    swState = GPIO.input(sw)
 
-    if clkState == 0:
-      while clkState != 1 or dtState != 1:
-        clkState = GPIO.input(clk)
+    if clState == 0:
+      while clState != 1 or dtState != 1:
+        clState = GPIO.input(cl)
         dtState = GPIO.input(dt)
       counter -= 1
       print counter
       select(url, counter%8)
     elif dtState == 0:
-      while clkState != 1 or dtState != 1:
-        clkState = GPIO.input(clk)
+      while clState != 1 or dtState != 1:
+        clState = GPIO.input(cl)
         dtState = GPIO.input(dt)
       counter += 1
       print counter
       select(url, counter%8)
+    elif swState == 0:
+      print 'Switch'
+      while swState == 0:
+        sleep(0.05)
+        swState = GPIO.input(sw)
 
 finally:
   GPIO.cleanup()
