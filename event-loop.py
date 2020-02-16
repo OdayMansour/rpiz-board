b6e00868b7d65772ac9f74d92d913b7ff01d6416
Oday Mansour <odaym@nsour.net>
2020-02-16 16:06:43 +0000

    Added state tracking and more background functions

--- event-loop.py
+++ event-loop.py
@@ -1,23 +1,114 @@
 from threading import Thread
 from time import sleep
-from random import randrange
+from os import system
 
-selection = 0
-
-def worker(name, sleeptime):
-    global selection
-    
-    print("Thread " + name + " started")
-    while True:
-        sleep(sleeptime)
-        selection = randrange(30)
-        print("Selection changed to " + str(selection) )
+import random
+import sys
 
 
-if __name__ == "__main__":
-    x = Thread(target=worker, args=('worker1',2,))
-    x.start()
 
-    while True:
-        sleep(0.1)
-        print(selection)
+##########################
+# State functions
+##########################
+
+def print_state(clear):
+    if clear:
+        system("clear")
+    for k in state:
+        print(k + " = " + str(state[k]))
+    print()
+
+
+def state_change():
+    for k in state:
+        if state[k] != previous_state[k]:
+            previous_state[k] = state[k]
+            return True
+    return False
+
+
+
+##########################
+# Background functions
+##########################
+
+def selector_tracker():
+    global state
+    global exit_flag
+
+    while not exit_flag:
+        state["selection"] = random.randrange(30)
+        sleep(random.uniform(0,4))
+
+
+def proximity_tracker():
+    global state
+    global exit_flag
+
+    while not exit_flag:
+        if (state["proximity"]):
+            sleep(0.5)
+            state["proximity"] = False
+        else:
+            sleep(random.uniform(0,2))
+            if (random.random() > 0.7):
+                state["proximity"] = True
+
+
+##########################
+# Variables
+##########################
+
+exit_flag = False
+
+state = {
+    "selection": 0,
+    "button": False,
+    "rain": False,
+    "proximity": False,
+    "calevent": "none"
+}
+previous_state = dict(state)
+
+
+
+##########################
+# Set up environment
+##########################
+
+random.seed()
+
+
+
+##########################
+# Background threads
+##########################
+
+thread_selector = Thread(target=selector_tracker)
+thread_proximity = Thread(target=proximity_tracker)
+# thread_rain = Thread(target=rain_tracker)
+# thread_calevent = Thread(target=calevent_tracker)
+
+thread_selector.start()
+thread_proximity.start()
+# thread_rain.start()
+# thread_calevent.start()
+
+
+
+##########################
+# Main loop
+##########################
+
+count = 0
+
+while True:
+    sleep(0.1)
+    if state_change():
+        print_state(True)
+    count += 1
+    if count > 150:
+        exit_flag = True
+        exit()
+
+
